da1d5ccfd714385fc29d4840d1310260cea1c89c
Adam Koblentz <adam.koblentz@zeroturnaround.com>
2014-09-24 10:45:24 -0400

    adding supporting files, MyJRebelBean.java and git_cleanup script

---
+++ MyJRebelBean.java
@@ -0,0 +1,14 @@
+package org.springframework.samples.petclinic;
+
+public class MyJRebelBean {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
